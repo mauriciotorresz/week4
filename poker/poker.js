8ee8852fcd93323a7af3a044ce6d215a6bb6c7b8
mauriciotorresz <mauricio.torres@kellogg.northwestern.edu>
2016-10-20 22:00:54 -0500

    Completion of Homework

--- poker/poker.js
+++ poker/poker.js
@@ -1,9 +1,40 @@
-// This code runs when the page loads
-$(function() {
+// shuffles (randomizes an array)
+// e.g. myArray.shuffle()
+Array.prototype.shuffle = function() {
+  var currentIndex = this.length, temporaryValue, randomIndex;
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = this[currentIndex];
+    this[currentIndex] = this[randomIndex];
+    this[randomIndex] = temporaryValue;
+  }
+  return this;
+}
 
-  $("#do-something").on("click", function(event) {
-    event.preventDefault()
-    $("#unicorn").slideToggle(2000)
+// returns a deck of cards
+// e.g. getDeck()
+window.getDeck = function() {
+  var ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king', 'ace'];
+  var suits = ['clubs', 'diamonds', 'hearts', 'spades'];
+  var cards = [];
+  ranks.forEach(function(rank, index) {
+    suits.forEach(function(suit, index) {
+      cards.push(rank + "_of_" + suit);
+    });
   });
+  return cards;
+}
 
+$(function() {
+  $("a").on("click", function(event) {
+    event.preventDefault()
+    var cards = window.getDeck()
+    cards.shuffle();
+    $("#c1").attr("src","http://golearntocode.com/images/cards/"+cards[0]+".png")
+    $("#c2").attr("src","http://golearntocode.com/images/cards/"+cards[1]+".png")
+    $("#c3").attr("src","http://golearntocode.com/images/cards/"+cards[2]+".png")
+    $("#c4").attr("src","http://golearntocode.com/images/cards/"+cards[3]+".png")
+    $("#c5").attr("src","http://golearntocode.com/images/cards/"+cards[4]+".png")
+})
 })
